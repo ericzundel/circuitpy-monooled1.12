b002fb9f06fdb6e2bcb16e3ee2e42d0404b534c3
Eric Ayers <ericzundel@gmail.com>
2022-02-06 14:06:26 -0500

    The bitmpa now looks correct

--- bmpinfo.py
+++ bmpinfo.py
@@ -12,7 +12,6 @@ import time
 class BMPInfoException(Exception):
     pass
 
-
 class bmpinfo:
 
     _BITMAPCOREHEADER_SIZE = 12
@@ -36,8 +35,8 @@ class bmpinfo:
         self._magic_number = chr(buffer[0]) + chr(buffer[1])
         if self._magic_number != "BM":
             raise BMPInfoException(
-                "bmpinfo.py: Unexpected magic number at beginning of BMP file: %s"
-                % buffer
+                "%s: Unexpected magic number at beginning of BMP file: %s"
+                % (__FILE__, buffer)
             )
 
         self._file_size = self._read_int32(file_handle)
@@ -65,25 +64,25 @@ class bmpinfo:
         elif self._bmp_header_size == bmpinfo._BITMAPV4HEADER_SIZE:
             self._read_bitmapv4header(file_handle)
         else:
-            raise BMPInfoException("Unhandled header size: %d" % self._bmp_header_size)
+            raise BMPInfoException("%s: Unhandled header size: %d" % (__FILE__, self._bmp_header_size))
 
         # Since we are only interested in monochrome bitmaps we can
         # do a validation
         if self._bits_per_pixel != 1:
             self.debug_info()
             raise BMPInfoException(
-                "bmpinfo.py: only handles monochrome bmp files. Got %d."
-                % self._bits_per_pixel
+                "%s: only handles monochrome bmp files. Got %d."
+                % (__FILE__, self._bits_per_pixel)
             )
         if self._compression != 0:
             self.debug_info()
             raise BMPInfoException(
-                "bmpinfo.py: only handles bmp files without compression. Got %d."
-                % self._compression
+                "$s: only handles bmp files without compression. Got %d."
+                % (__FILE__, self._compression)
             )
         if self._width <= 0:
             self.debug_info()
-            raise BMPInfoException("bmpinfo.py: Can't handle width: %d" % self._width)
+            raise BMPInfoException("%s: Can't handle width: %d" % (__FILE__, self._width))
         # Since the header can be different sizes in different version of BMP file,
         # calling seek()  will position the next read at the beginning of the data.
         file_handle.seek(self._data_offset, 0)
@@ -94,7 +93,7 @@ class bmpinfo:
     def _read_bitmap_data(self, file_handle):
         self._bitmap_data = [[] for i in range(self._height)]
         for row in range(0, abs(self._height)):
-            self._bitmap_data[row] = [0 for i in range(self._width)]
+            self._bitmap_data[row] = bytearray(b'\x00'*self._width)
         # Calculate the # of bytes to the nearest 4 byte boundary that make up a row.
         # The +31 is a tricky way to get around the remaining bits not evenly divisible by 4
         row_length_bytes = (int((float(self._width) + 31.0) / 32.0)) * 4
@@ -103,6 +102,8 @@ class bmpinfo:
             buffer = file_handle.read(row_length_bytes)
             if not buffer:
                 break
+            # Swap the order of the 32 bit integers in the buffer.
+            buffer = self._reorder_buffer(buffer)
             column = 0
             for byte_val in buffer:
                 if column >= self._width:
@@ -116,6 +117,38 @@ class bmpinfo:
                     #    print("0 found at %d, %d" % (row, column))
                     #    break
             row = row + 1
+
+        if (self._height > 0):
+            self._bitmap_data.reverse()
+
+    def _reorder_buffer(self, buffer):
+        # Do reordering from little endien to big endien. Assumes buffer is an increment of 4
+        if len(buffer) % 4 != 0:
+            raise BMPInfoException("%s: Expected buffer length a multiple of 4, got %d" % (__FILE__, len(buffer)))
+
+        output_buffer = bytearray(b'\x00'*len(buffer))
+
+        for i in range(0, len(buffer) / 4):
+            offset = i*4
+            num = int(buffer[offset +3]) + int(buffer[offset+2] << 8) + int(buffer[offset+1] << 16) + int(buffer[offset] << 24)
+            num = self._reverse_bits(num)
+            # store the data back in the array
+            output_buffer[offset] = (num) & 0xff
+            output_buffer[offset + 1] = (num >> 8) & 0xff
+            output_buffer[offset + 2] = (num >> 16) & 0xff
+            output_buffer[offset + 3] = (num >> 24) & 0xff
+
+        return output_buffer
+
+    def _reverse_bits(self, orig_num):
+        # reverse the bits
+        #num = 0
+        #for j in range(0,31):
+        #    num += int((orig_num >> (31-j)) & 0x1) << (j+1)
+        #return num
+
+        # Cheating: https://stackoverflow.com/questions/5333509/how-do-you-reverse-the-significant-bits-of-an-integer-in-python
+        return sum(1<<(32-1-i) for i in range(32) if orig_num>>i&1)
 
     def _read_bitmapcoreheader(self, file_handle):
         self._width = self._read_int16(file_handle)  # Only 2^16? Scandalous.
